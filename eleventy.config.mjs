3207be3fb4d56034e5e95f7818fc7fd9fc59c748
Loïc Boiteux <loicboiteux@protonmail.com>
2025-06-17 23:09:34 -0400

    Added SCSS and other things

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -3,10 +3,12 @@ import path from 'path';
 import postcss from 'postcss';
 import tailwindcss from '@tailwindcss/postcss';
 
+import { I18nPlugin } from "@11ty/eleventy";
+
 export default function (eleventyConfig) {
 	eleventyConfig.on('eleventy.before', async () => {
 		const tailwindInputPath = path.resolve('./src/assets/css/tailwind.css');
-		const tailwindOutputPath = './dist/assets/css/styles.css';
+		const tailwindOutputPath = './dist/assets/css/tailwind.css';
 		const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
 		const outputDir = path.dirname(tailwindOutputPath);
 
@@ -20,6 +22,25 @@ export default function (eleventyConfig) {
 		});
 
 		fs.writeFileSync(tailwindOutputPath, result.css);
+	});
+
+	eleventyConfig.addPlugin(I18nPlugin, {
+		// any valid BCP 47-compatible language tag is supported
+		defaultLanguage: "fr", // Required
+
+		// Rename the default universal filter names
+		filters: {
+			// transform a URL with the current page’s locale code
+			url: "locale_url",
+
+			// find the other localized content for a specific input file
+			links: "locale_links",
+		},
+
+		// When to throw errors for missing localized content files
+		errorMode: "strict", // throw an error if content is missing at /en/slug
+		// errorMode: "allow-fallback", // only throw an error when the content is missing at both /en/slug and /slug
+		// errorMode: "never", // don’t throw errors for missing content
 	});
 
 	return {
